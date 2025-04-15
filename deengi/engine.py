9d56acc55cf4c00b5482781ae1922f8eaac91eb7
simonschaluppe <simon.schneider@technikum-wien.at>
2025-04-15 10:58:42 +0200

    debug layer and default keybods [Q]uit, [d]ebug mode, [P]ause

--- deengi/engine.py
+++ deengi/engine.py
@@ -21,36 +21,37 @@ class Engine:
             "main": [],
             "ui": [],
             "overlay": [],
+            "debug": [],
         }
         self.layer_visibility = {
             "background": True,
             "main": True,
             "ui": True,
             "overlay": True,  # Tooltips or dialogs can default to hidden
+            "debug": debug,
         }
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(screen_size)
         pygame.display.set_caption(title)
-        
+
         self.camera = Camera2D(self.screen, zoom=(1, 1), debug=self.debugmode)
         pygame.screen_coords = self.camera.screen_coords
-        
+
         self.renderer = Renderer(self.screen, camera=self.camera, debug=self.debugmode)
-        
+
         self.input_handler = InputHandler(
             screen_coords=self.camera.screen_coords, debug=self.debugmode
         )
         self.setup_camera()
         self.show_background()
-        
+
         self.paused = False
-        self.bind_key("q", self.quit)
-        self.bind_key("d", self.toggle_debug)
-        self.bind_key("p", self.toggle_pause)
-        
-    
+        self.bind_key("q", self.quit, "Quit")
+        self.bind_key("d", self.toggle_debug, "toggle debug mode")
+        self.bind_key("p", self.toggle_pause, "Pause")
+
         self.update_callbacks = []
-        
+
     def setup_camera(
         self,
         rotation=0,
@@ -85,7 +86,7 @@ class Engine:
             if not self.paused:
                 self.update()
             self.input_handler.update()
-            for layer_name in ["background", "main", "ui", "overlay"]:
+            for layer_name in ["background", "main", "ui", "overlay", "debug"]:
                 if not self.layer_visibility[layer_name]:
                     continue
                 for renderable in self.layers[layer_name]:
@@ -95,18 +96,29 @@ class Engine:
                         callback, data_dict = renderable
                         callback(**data_dict)
 
-                
             if self.debugmode:  # putnthis in overlay
                 self.renderer.draw_debug()
             # render calls
             self.screen.blit(self.renderer.display, (0, 0))
             pygame.display.update()
 
-    def clear_layer(self, layer):
+    def clear_layer(self, layer=None):
         self.layers[layer] = []
 
+    def clear(self, *layers):
+        if not layers:
+            layers = self.layers
+        for l in layers:
+            if l in self.layers:
+                self.layers[l] = []
+            else:
+                raise KeyError(f"layer {l=} not in {self.layers=}")
+
     def add_to_layer(self, layer="main", *renderables):
-        if layer not in self.layers: raise KeyError(f"Layer {layer} not found in engine Layers: {list(self.layers.keys())}")
+        if layer not in self.layers:
+            raise KeyError(
+                f"Layer {layer} not found in engine Layers: {list(self.layers.keys())}"
+            )
         for renderable in renderables:
             self.layers[layer].append(renderable)
 
@@ -115,8 +127,8 @@ class Engine:
         self.add_to_layer("overlay", tooltip)
         self.input_handler.register_hover(renderable, tooltip.set_hover)
 
-    def show_debug(self, callback):
-        self.renderer.debug_statements.append(callback)
+    def show_debug(self, statement):
+        self.renderer.debug_statements.append(statement)
 
     def show_scene(self, scene):
         self.clear_layer("ui")
@@ -130,24 +142,30 @@ class Engine:
         self.input_handler.bind_options_to_keys(scene.options)
         self.add_to_layer("ui", scene)
 
-    def show_background(self, color=(0,0,0)):
+    def show_background(self, color=(0, 0, 0)):
         self.clear_layer("background")
         self.layers["background"].append((self.renderer.draw_bg, dict(color=color)))
 
-    def show_grid(self, lines:int|tuple[int,int]=20, start:tuple[int,int]=None, **kwargs):
+    def show_grid(
+        self, lines: int | tuple[int, int] = 20, start: tuple[int, int] = None, **kwargs
+    ):
         if isinstance(lines, int):
             xlines = ylines = lines
         elif isinstance(lines, tuple):
             xlines, ylines = lines
-        else: raise ValueError(f"Argument lines must be int or tuple of ints")
+        else:
+            raise ValueError(f"Argument lines must be int or tuple of ints")
         if not start:
             startx = -xlines // 2
             starty = -ylines // 2
         elif isinstance(start, tuple):
             startx, starty = start
-        else: raise ValueError(f"Argument start must be tuple of ints")
-              
-        grid = renderables.Grid((startx, startx+xlines), (starty, starty+ylines), **kwargs)
+        else:
+            raise ValueError(f"Argument start must be tuple of ints")
+
+        grid = renderables.Grid(
+            (startx, startx + xlines), (starty, starty + ylines), **kwargs
+        )
         self.add_to_layer(
             "background",
             grid,
@@ -170,8 +188,14 @@ class Engine:
     def toggle_pause(self):
         self.paused = not self.paused
 
-    def bind_key(self, key, callback):
-        self.input_handler.bind_keypress(ord(key.lower()), callback)
+    def bind_key(self, key, callback, binding_name=None):
+        key = str(key) if type(key) is not str else key
+        self.input_handler.bind_keypress(
+            ord(key.lower()), callback, binding_name=binding_name
+        )
+
+    def get_keybinds(self):
+        return self.input_handler.get_keybinds()
 
     def quit(self):
         pygame.quit()
